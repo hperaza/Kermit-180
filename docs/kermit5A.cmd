647e461380e75cb77e3060b1fde7da16244cc456
Hector Peraza <hperaza65@gmail.com>
2021-01-10 20:25:31 +0100

    Added example TAKE files for (Linux) C-Kermit versions 5 and 9.

---
+++ docs/kermit5A.cmd
@@ -0,0 +1,5 @@
+set line /dev/ttyUSB1
+set speed 19200
+set parity space
+set file type binary
+set control-character prefixed all
---
+++ docs/kermit9.cmd
@@ -0,0 +1,13 @@
+set line /dev/ttyUSB1
+set speed 19200
+set carrier-watch off
+set flow-control none
+set parity space
+set attributes off
+set file type binary
+set file patterns off
+set transfer mode manual
+set control-character prefixed all
+show comm
+show protocol
+show file
